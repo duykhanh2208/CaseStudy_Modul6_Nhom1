1b88e8f8d239e55c8e40f4a4620fce5e4a4814f0
duykhanh2208 <khanh.ld2208@gmail.com>
2023-12-05 16:39:15 +0700

    Upload Booking Controller

---
+++ src/main/java/com/houserenting/controller/BookingController.java
@@ -0,0 +1,59 @@
+package com.houserenting.controller;
+
+import com.houserenting.model.Booking;
+import com.houserenting.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/booking")
+public class BookingController {
+    @Autowired
+    private BookingService bookingServiceImpl;
+
+    @GetMapping("/bookings")
+    public ResponseEntity<Iterable<Booking>> showList(){
+        return new ResponseEntity<>(bookingServiceImpl.showAll(), HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Booking>> showOne(@PathVariable Long id){
+        Optional<Booking> booking = bookingServiceImpl.findOne(id);
+        if(booking.isPresent()){
+            return new ResponseEntity<>(booking,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PostMapping("")
+    public ResponseEntity<Booking> create(@RequestBody Booking booking){
+        bookingServiceImpl.save(booking);
+        return new ResponseEntity<>(booking,HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Booking> save(@PathVariable Long id, @RequestBody Booking booking){
+        Optional<Booking> oldBooking = bookingServiceImpl.findOne(id);
+        if(oldBooking.isPresent()){
+            booking.setId(oldBooking.get().getId());
+            bookingServiceImpl.save(booking);
+            return new ResponseEntity<>(booking,HttpStatus.ACCEPTED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> delete(@PathVariable Long id){
+        Optional<Booking> booking = bookingServiceImpl.findOne(id);
+        if(booking.isPresent()){
+            bookingServiceImpl.delete(id);
+            return new ResponseEntity<>(id,HttpStatus.ACCEPTED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
