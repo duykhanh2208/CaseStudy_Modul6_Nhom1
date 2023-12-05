878ab17f108e077cbc59fb0214471e1ab863ef3e
duykhanh2208 <khanh.ld2208@gmail.com>
2023-12-05 13:58:06 +0700

    fix GeneralService

--- src/main/java/com/houserenting/service/GeneralService.java
+++ src/main/java/com/houserenting/service/GeneralService.java
@@ -5,6 +5,6 @@ import java.util.Optional;
 public interface GeneralService<E>{
     Iterable<E> showAll();
     Optional<E> findOne(Long id);
-    Optional<E> save(E e);
+    E save(E e);
     Long delete(Long id);
 }
